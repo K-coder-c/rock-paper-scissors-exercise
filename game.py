51f253c4c86654de9cf958fdd13c06884531a8ab
K-coder-c <kca48@georgetown.edu>
2021-09-13 20:36:01 -0400

    Added Welcome banner, player input, and player output choice

---
+++ game.py
@@ -0,0 +1,26 @@
+# game.py
+
+#print("Rock, Paper, Scissors, Shoot!")
+
+#First Prompt User to input "Rock, Paper, or Scissors"
+
+print("*************") #13 stars
+
+print("Welcome 'Player One' to my Rock, Paper, Scissors Game :)")
+
+print("*************") #13 stars
+
+#Next Compare the user selection agaisnt the list of valid options
+
+print("Please Input either Rock, Paper, or Scissors:")
+
+print("*************") #13 stars
+
+x = input()
+
+print("*************") #13 stars
+
+#use an if statment here
+
+print("You Chose: " +x)
+
