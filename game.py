21c139ced7743b08a5e7a2d06350432463130894
K-coder-c <kca48@georgetown.edu>
2021-09-16 12:09:14 -0400

    added computer random input

--- game.py
+++ game.py
@@ -4,43 +4,45 @@
 
 #First Prompt User to input "Rock, Paper, or Scissors"
 
-print("*************") #13 stars
+#print("*************") #13 stars
 
 print("Welcome 'Player One' to my Rock, Paper, Scissors Game :)")
 
-print("*************") #13 stars
+#print("*************") #13 stars
 
 #Next Compare the user selection agaisnt the list of valid options
 
-print("Please Input either Rock, Paper, or Scissors:")
+#print("Please Input either Rock, Paper, or Scissors:")
 
-print("*************") #13 stars
+#print("*************") #13 stars
 
 #defined variable
 
-x = input()
+user_chose = input("Please Input either Rock, Paper, or Scissors:")
 
-print("*************") #13 stars
+#print("*************") #13 stars
 
 #use an if statment here
 
-print("You Chose: " +x)
+print(user_chose)
 
-print("*************") #13 stars
+#print("You Chose: " +x)
+
+#print("*************") #13 stars
 
 #checks to see if user entered valid input
 
-if input == "Rock" or "rock" :
+if input == "Rock" and "rock" : #changed or to and to see if that works
 
-    print("You chose: " +x)
+    print("You chose: " +user_chose)
 
 elif input == "Paper" or "paper" :
 
-    print("You chose: " +x)
+    print("You chose: " +user_chose) #this might be the problem..
 
 elif input == "Scissors" or "scissors" :
 
-    print("You chose: " +x)
+    print("You chose: " +user_chose)
 
 else :
     print("You have entered an Inaccurate Input!")
