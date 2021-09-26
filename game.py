adf4ee326b87cd963a8a0f9986328068fbfe43d8
K-coder-c <kca48@georgetown.edu>
2021-09-26 12:32:10 -0400

    I am trying to figure out the player_name part of the assignment but I have already created the .env file and its contexts...just not sure what to do next

--- game.py
+++ game.py
@@ -1,10 +1,6 @@
 # game.py
 
-
-
 import random
-
-
 
 #print("Rock, Paper, Scissors, Shoot!")
 
@@ -24,13 +20,13 @@ print("Welcome 'Player One' to my Rock, Paper, Scissors Game :)")
 
 #defined variable
 
-user_chose = input("Please Input either Rock, Paper, or Scissors:")
+user_choice = input("Please Input either Rock, Paper, or Scissors:")
 
 #print("*************") #13 stars
 
 #use an if statment here
 
-print("You chose: " +user_chose)
+print("You chose: " +user_choice)
 
 #print("You Chose: " +x)
 
@@ -46,19 +42,33 @@ computer_choice = random.choice(options)
 print("The computer chose: " +computer_choice)
 
 
+if user_choice == computer_choice : 
 
-#if input == "Rock" and "rock" : #changed or to and to see if that works
+    print("It was a tie!")
 
-    #print("You chose: " +user_chose)
+elif user_choice == "rock" and computer_choice == "paper" :
 
-#elif input == "Paper" or "paper" :
+    print("The Computer won!")
 
-    #print("You chose: " +user_chose) #this might be the problem..
+elif user_choice == "scissors" and computer_choice == "paper" :
 
-#elif input == "Scissors" or "scissors" :
+    print("You won!")
 
-    #print("You chose: " +user_chose)
+elif user_choice == "paper" and computer_choice == "rock" :
+    print("You won")
 
-#else :
-    #print("You have entered an Inaccurate Input!")
+elif user_choice == "rock" and computer_choice == "Scissors" :
+    print("You won!")
 
+elif computer_choice == "scissors" and user_choice == "paper" :
+    print("The Computer Won!")
+
+elif computer_choice == "rock" and user_choice == "scissors" :
+    print("The Computer Won!")
+
+#breakpoint()
+
+if user_choice != "rock" or "paper" or "scissors" : #it works :))))
+    print("ERROR INCORRECT INPUT/OUTPUT")
+
+print("Thanks, Please Play Again")
